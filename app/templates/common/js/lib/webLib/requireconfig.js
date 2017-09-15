4214d7da7925684895d1a0ea06b788265344cd0e
shusc24 <shusc234@hotmail.com>
2017-09-15 08:23:06 +0800

    modification

--- app/templates/common/js/lib/webLib/requireconfig.js
+++ app/templates/common/js/lib/webLib/requireconfig.js
@@ -4,7 +4,7 @@ requirejs.config({
     urlargs:"v=1.00",
     paths: {
         //核心库
-        jquery: 'https://cdn.bootcss.com/jquery/3.2.1/jquery.min',
+        jquery: 'https://cdn.bootcss.com/jquery/2.2.4/jquery.min',
         jqueryOne: 'https://cdn.bootcss.com/jquery/1.12.4/jquery.min',
         Vue: "https://cdn.bootcss.com/vue/2.4.2/vue.min",
         VueOne: 'https://cdn.bootcss.com/vue/1.0.28/vue.min',
@@ -16,9 +16,9 @@ requirejs.config({
         lodashCore: 'https://cdn.bootcss.com/underscore.js/1.8.3/underscore-min',
 
         // Vue插件
-        Vuex: "https://cdn.bootcss.com/vuex/2.3.1/vuex.min",
-        Vresource: ["https://cdn.bootcss.com/vue-resource/1.3.1/vue-resource.min"],
-        Vrouter: ["https://cdn.bootcss.com/vue-router/2.5.2/vue-router.min"],
+        Vuex: "https://cdn.bootcss.com/vuex/2.4.0/vuex.min",
+        Vresource: ["https://cdn.bootcss.com/vue-resource/1.3.4/vue-resource.min"],
+        Vrouter: ["https://cdn.bootcss.com/vue-router/2.7.0/vue-router.min"],
 
         core: "lib/webLib/public/core/core",
         h5core: "lib/webLib/public/core/H5core",
@@ -26,13 +26,13 @@ requirejs.config({
         //插件库
         swiper: "https://cdn.bootcss.com/Swiper/3.4.2/js/swiper.min",
         swiper2: "https://cdn.staticfile.org/Swiper/2.7.6/idangerous.swiper", //兼容ie6 ie7
-        echarts: "https://cdn.staticfile.org/echarts/3.3.2/echarts.min",
+        echarts: "https://cdn.bootcss.com/echarts/3.7.1/echarts.min",
         sweetalert: 'https://cdn.bootcss.com/sweetalert/1.1.3/sweetalert.min',
-        weixin: 'http://res.wx.qq.com/open/js/jweixin-1.1.0',
+        wx: 'http://res.wx.qq.com/open/js/jweixin-1.1.0',
         popper: 'https://cdn.bootcss.com/popper.js/1.0.8/popper.min',
         cropper: "https://cdn.bootcss.com/cropper/3.0.0-alpha.1/cropper.min",//图片裁剪插件
         layer: "https://cdn.bootcss.com/layer/3.0.1/layer.min",
-        socketIo: 'https://cdn.bootcss.com/socket.io/1.7.3/socket.io.min.',
+        socketIo: 'https://cdn.bootcss.com/socket.io/2.0.3/socket.io.slim',
         dynamics: 'https://cdn.staticfile.org/dynamics.js/1.1.5/dynamics.min',//物理库
 
         //前端路由 文档地址 https://github.com/flatiron/director
@@ -42,8 +42,8 @@ requirejs.config({
         pagination: 'lib/webLib/public/plus/pagination/lib/jquery.pagination',
         wangEditor: "lib/webLib/public/plus/wangEditor/js/wangEditor.min",
         lazyload: 'lib/webLib/public/plus/lazyload/lazyload.min',
-        store: 'lib/webLib/public/plus/store/store.min',
-        owlCarousel: 'lib/Welib/public/plus/owlCarousel/owl.carousel',
+        store: 'https://cdn.bootcss.com/store.js/1.3.20/store.min',
+        owlCarousel: 'https://cdn.bootcss.com/OwlCarousel2/2.2.1/owl.carousel.min',
 
         //表单验证插件 文档地址：https://github.com/WLDragon/SMValidator
         SMV: 'lib/webLib/public/plus/SMValidator/SMValidator.min',
@@ -70,10 +70,10 @@ requirejs.config({
         xregexp: 'https://cdn.bootcss.com/xregexp/3.1.1/xregexp-all.min',
 
         //anime 动画库 官方文档：http://anime-js.com/ https://github.com/juliangarnier/anime
-        anime: 'https://cdn.bootcss.com/animejs/2.0.0/anime.min',
+        anime: 'https://cdn.bootcss.com/animejs/2.0.2/anime.min',
 
         //复制到粘贴板
-        clipboard: 'https://cdn.bootcss.com/clipboard.js/1.6.1/clipboard.min',
+        clipboard: 'https://cdn.bootcss.com/clipboard.js/1.7.1/clipboard.min',
 
         //单页面切换
         mobilebone: 'https://cdn.bootcss.com/mobilebone/2.6.1/mobilebone.min',
@@ -82,7 +82,7 @@ requirejs.config({
         cookies: 'https://cdn.bootcss.com/js-cookie/2.1.3/js.cookie.min',
 
         //input格式化库 github文档 https://github.com/nosir/cleave.js 官网：http://nosir.github.io/cleave.js/
-        cleave: 'https://cdn.bootcss.com/cleave.js/0.7.15/cleave.min',
+        cleave: 'https://cdn.bootcss.com/cleave.js/1.0.1/cleave.min',
 
         //动画库兼容IE8  http://velocityjs.org/
         velocity: 'https://cdn.bootcss.com/velocity/1.5.0/velocity.min',
@@ -93,9 +93,9 @@ requirejs.config({
 
         LCalendar: 'lib/weblib/public/plus/LCalendar/src/js/LCalendar',
 
-        Vee: 'https://cdn.bootcss.com/vee-validate/2.0.0-beta.25/vee-validate.min',
+        Vee: 'https://cdn.bootcss.com/vee-validate/2.0.0-rc.14/vee-validate.min',
 
-        VeeCn: "https://cdn.bootcss.com/vee-validate/2.0.0-beta.25/locale/zh_CN",
+        VeeCn: "https://cdn.bootcss.com/vee-validate/2.0.0-rc.14/locale/zh_CN",
 
         // 用户组件
         imgUpload: 'lib/webLib/user/shusc/jqueryUploadimg/jqueryUploadimg',
@@ -124,10 +124,13 @@ requirejs.config({
         artTemplate:'lib/webLib/public/plus/artTemplate/artTemplate',
 
         velocityUi:'https://cdn.bootcss.com/velocity/1.5.0/velocity.ui.min',
-
+        //相册插件
         smartPhoto:"lib/webLib/public/plus/smartphoto/js/smartphoto.min",
+        //跑马灯插件
         marquee:"https://cdn.bootcss.com/jQuery.Marquee/1.3.94/jquery.marquee.min",
+        //元素固定
         scrollfix:'lib/webLib/public/plus/scrollfix/scrollfix.min',
+
         scrollbar:"https://cdn.bootcss.com/jquery.perfect-scrollbar/0.8.0/js/perfect-scrollbar.min"
     },
     shim: {
