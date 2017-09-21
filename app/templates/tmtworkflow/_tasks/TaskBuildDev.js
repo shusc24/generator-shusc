faf329d64d818a7fc60afe5e916b711d5407e483
chen <1378116863@qq.com>
2017-09-21 09:17:42 +0800

    修改测试

--- app/templates/tmtworkflow/_tasks/TaskBuildDev.js
+++ app/templates/tmtworkflow/_tasks/TaskBuildDev.js
@@ -76,7 +76,7 @@ module.exports = function(gulp, config) {
         config.livereload && bs.reload();
     };
 
-    //清除目标目录1
+    //清除目标目录12
     function delDev() {
         return del([paths.dev.dir]);
     }
