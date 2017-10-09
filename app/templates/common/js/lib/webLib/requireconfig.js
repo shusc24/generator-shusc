cbc617e6a17c69fceed846a773bf063cd06b8e67
shusc24 <shusc234@hotmail.com>
2017-10-09 10:20:34 +0800

    修改模板

--- app/templates/common/js/lib/webLib/requireconfig.js
+++ app/templates/common/js/lib/webLib/requireconfig.js
@@ -14,7 +14,7 @@ requirejs.config({
         lodashCore: 'https://cdn.bootcss.com/underscore.js/1.8.3/underscore-min',
 
         // Vue插件
-        Vuex: "https://cdn.bootcss.com/vuex/2.4.0/vuex.min",
+        Vuex: "https://cdn.bootcss.com/vuex/2.4.1/vuex.min",
         Vresource: ["https://cdn.bootcss.com/vue-resource/1.3.4/vue-resource.min"],
         Vrouter: ["https://cdn.bootcss.com/vue-router/2.7.0/vue-router.min"],
 
@@ -29,7 +29,7 @@ requirejs.config({
         wx: 'http://res.wx.qq.com/open/js/jweixin-1.1.0',
         popper: 'https://cdn.bootcss.com/popper.js/1.0.8/popper.min',
         cropper: "https://cdn.bootcss.com/cropper/3.0.0-alpha.1/cropper.min",//图片裁剪插件
-        layer: "https://cdn.bootcss.com/layer/3.0.1/layer.min",
+        layer: "https://cdn.bootcss.com/layer/3.0.3/layer.min",
         socketIo: 'https://cdn.bootcss.com/socket.io/2.0.3/socket.io.slim',
         dynamics: 'https://cdn.staticfile.org/dynamics.js/1.1.5/dynamics.min',//物理库
 
@@ -38,7 +38,7 @@ requirejs.config({
 
         zui: "lib/webLib/public/plus/zui/zui",
         pagination: 'lib/webLib/public/plus/pagination/lib/jquery.pagination',
-        wangEditor: "lib/webLib/public/plus/wangEditor/js/wangEditor.min",
+        wangEditor: "https://cdn.bootcss.com/wangEditor/3.0.10/wangEditor.min",
         lazyload: 'lib/webLib/public/plus/lazyload/lazyload.min',
         store: 'https://cdn.bootcss.com/store.js/1.3.20/store.min',
         owlCarousel: 'https://cdn.bootcss.com/OwlCarousel2/2.2.1/owl.carousel.min',
@@ -51,7 +51,7 @@ requirejs.config({
         text: "https://cdn.bootcss.com/require-text/2.0.12/text.min",
         domReady: "https://cdn.bootcss.com/require-domReady/2.0.1/domReady.min",
         css: 'https://cdn.bootcss.com/require-css/0.1.10/css.min',
-        backboneLocal: 'https://cdn.staticfile.org/backbone-localstorage.js/1.1.16/backbone.localStorage-min',
+        backboneLocal: 'https://cdn.bootcss.com/backbone-localstorage.js/1.1.16/backbone.localStorage-min',
         //h5 手势库
         AlloyFinger: 'lib/webLib/public/plus/AlloyFinger/alloy_finger',
 
@@ -59,16 +59,16 @@ requirejs.config({
         flow: 'lib/webLib/public/plus/layuiflow/flow',
 
         //滚动动画插件 官方文档: https://github.com/jlmakes/scrollreveal
-        scrollreveal: 'https://cdn.bootcss.com/scrollReveal.js/3.3.2/scrollreveal.min',
+        scrollreveal: 'https://cdn.bootcss.com/scrollReveal.js/4.0.0-beta.15/scrollreveal.min',
 
         //iscroll 自定义滚动条 https://github.com/cubiq/iscroll
         iscroll: 'https://cdn.bootcss.com/iScroll/5.2.0/iscroll.min',
 
         //正则库 官方文档：https://github.com/slevithan/xregexp
-        xregexp: 'https://cdn.bootcss.com/xregexp/3.1.1/xregexp-all.min',
+        xregexp: 'https://cdn.bootcss.com/xregexp/3.2.0/xregexp-all.min',
 
         //anime 动画库 官方文档：http://anime-js.com/ https://github.com/juliangarnier/anime
-        anime: 'https://cdn.bootcss.com/animejs/2.0.2/anime.min',
+        anime: 'https://cdn.bootcss.com/animejs/2.2.0/anime.min',
 
         //复制到粘贴板
         clipboard: 'https://cdn.bootcss.com/clipboard.js/1.7.1/clipboard.min',
@@ -77,7 +77,7 @@ requirejs.config({
         mobilebone: 'https://cdn.bootcss.com/mobilebone/2.6.1/mobilebone.min',
 
         //cookies 工具 github文档:https://github.com/js-cookie/js-cookie
-        cookies: 'https://cdn.bootcss.com/js-cookie/2.1.3/js.cookie.min',
+        cookies: 'https://cdn.bootcss.com/js-cookie/2.1.4/js.cookie.min',
 
         //input格式化库 github文档 https://github.com/nosir/cleave.js 官网：http://nosir.github.io/cleave.js/
         cleave: 'https://cdn.bootcss.com/cleave.js/1.0.1/cleave.min',
@@ -87,13 +87,13 @@ requirejs.config({
 
         handlebars: "https://cdn.bootcss.com/handlebars.js/4.0.10/handlebars.min",
 
-        validator: "https://cdn.bootcss.com/validator/7.0.0/validator.min",
+        validator: "https://cdn.bootcss.com/validator/9.0.0/validator.min",
 
         LCalendar: 'lib/weblib/public/plus/LCalendar/src/js/LCalendar',
 
-        Vee: 'https://cdn.bootcss.com/vee-validate/2.0.0-rc.14/vee-validate.min',
+        Vee: 'https://cdn.bootcss.com/vee-validate/2.0.0-rc.18/vee-validate.min',
 
-        VeeCn: "https://cdn.bootcss.com/vee-validate/2.0.0-rc.14/locale/zh_CN",
+        VeeCn: "https://cdn.bootcss.com/vee-validate/2.0.0-rc.18/locale/zh_CN",
 
         // 用户组件
         imgUpload: 'lib/webLib/user/shusc/jqueryUploadimg/jqueryUploadimg',
@@ -129,8 +129,9 @@ requirejs.config({
         //元素固定
         scrollfix:'lib/webLib/public/plus/scrollfix/scrollfix.min',
 
-        scrollbar:"https://cdn.bootcss.com/jquery.perfect-scrollbar/0.8.0/js/perfect-scrollbar.min",
+        scrollbar:"https://cdn.bootcss.com/jquery.perfect-scrollbar/0.8.1/js/perfect-scrollbar.min",
 
+        // 流式布局
         masonry:"https://cdn.bootcss.com/masonry/4.2.0/masonry.pkgd.min",
 
         layoutFlow:"lib/webLib/user/shusc/flowlayout/flowlayout",
@@ -142,7 +143,7 @@ requirejs.config({
         photoSwiperClass:"lib/webLib/user/shusc/photoSwiper/photoSwiperClass"
     },
     shim: {
-        layer: ['jquery', 'css!https://cdn.bootcss.com/layer/3.0.1/skin/default/layer.min.css'],
+        layer: ['jquery', 'css!https://cdn.bootcss.com/layer/3.0.3/skin/default/layer.min.css'],
         zui: ['jquery'],
         AlloyFinger: {
             exports: 'AlloyFinger'
@@ -159,8 +160,9 @@ requirejs.config({
         swiperScrollbar:['swiper2'],
         smartPhoto:['css!../other/smartPhoto/smartphoto.min.css'],
         marquee:['jquery'],
-        scrollbar:['css!https://cdn.bootcss.com/jquery.perfect-scrollbar/0.8.0/css/perfect-scrollbar.min.css'],
+        scrollbar:['css!https://cdn.bootcss.com/jquery.perfect-scrollbar/0.8.1/css/perfect-scrollbar.min.css'],
         photoSwiper:['css!http://lib.baomitu.com/photoswipe/4.1.2/photoswipe.min.css'],
-        photoSwiperUi:['css!http://lib.baomitu.com/photoswipe/4.1.2/default-skin/default-skin.min.css']
+        photoSwiperUi:['css!http://lib.baomitu.com/photoswipe/4.1.2/default-skin/default-skin.min.css'],
+        wangEditor:['css!https://cdn.bootcss.com/wangEditor/3.0.10/wangEditor.min.css']
     }
 });
