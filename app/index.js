1e98f3bcf338a7a0dd34fe672428cd81da561d2c
shusc24 <shusc234@qq.com>
2018-01-31 22:26:28 +0800

    更新配置 调整依赖 引入ejs 和 ejs-

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ var Generator = require('yeoman-generator'),
     log = console.log,
     fs = require('fs'),
     path = require('path'),
-    generatorName = 'gulp';  
+    generatorName = 'gulp';
 module.exports = class extends Generator {
-    constructor(args, opts){
+    constructor(args, opts) {
         super(args, opts);
     }
-    prompting(){
+
+    prompting() {
         var questions = [
             {
                 name: 'projectAssets',
@@ -18,18 +19,18 @@ module.exports = class extends Generator {
                         name: '标准pc模板',
                         value: 'pc',
                         checked: true   // 默认选中1
-                    },{
+                    }, {
                         name: '标准Mobile模板',
                         value: 'mobile'
-                    },{
-                        name:"简单pc模板",
+                    }, {
+                        name: "简单pc模板",
                         value: 'spc',
-                    },{
-                        name:"简单Mobile模板",
+                    }, {
+                        name: "简单Mobile模板",
                         value: 'smobile',
-                    },{
-                        name:"workflow模板",
-                        value:"workflow"
+                    }, {
+                        name: "workflow模板",
+                        value: "workflow"
                     }
                 ]
             },
@@ -42,45 +43,47 @@ module.exports = class extends Generator {
         ];
         return this.prompt(questions).then(
             (answers) => {
-                for(var item in answers){
+                for (var item in answers) {
                     // 把answers里的内容绑定到外层的this，便于后面的调用
                     answers.hasOwnProperty(item) && (this[item] = answers[item]);
                 }
             });
     }
-    writing(){
+
+    writing() {
         //复制tmtworkflow
-        if(this.projectAssets === "workflow"){
-            this.fs.copy(this.templatePath('tmtworkflow'),this.destinationPath(`${this.projectName}`));
+        if (this.projectAssets === "workflow") {
+            this.fs.copy(this.templatePath('tmtworkflow'), this.destinationPath(`${this.projectName}`));
             //复制workflow 文件
-            this.fs.copy(this.templatePath('tmtworkflow/_.tmtworkflowrc'),this.destinationPath(`${this.projectName}/.tmtworkflowrc`));
+            this.fs.copy(this.templatePath('tmtworkflow/_.tmtworkflowrc'), this.destinationPath(`${this.projectName}/.tmtworkflowrc`));
         }
-        else if(this.projectAssets === "mobile" || this.projectAssets === "pc"){
+        else if (this.projectAssets === "mobile" || this.projectAssets === "pc") {
             //复制模板
-            this.fs.copy(this.templatePath( `${this.projectAssets}`),this.destinationPath(`${this.projectName}`));
+            this.fs.copy(this.templatePath(`${this.projectAssets}`), this.destinationPath(`${this.projectName}`));
             // 复制js
-            this.fs.copy(this.templatePath('common/js'),this.destinationPath(`${this.projectName}/src/js`));
+            this.fs.copy(this.templatePath('common/js'), this.destinationPath(`${this.projectName}/src/js`));
 
             //复制css
-            this.fs.copy(this.templatePath('common/css'),this.destinationPath(`${this.projectName}/src/css`));
+            this.fs.copy(this.templatePath('common/css'), this.destinationPath(`${this.projectName}/src/css`));
             //复制babel 设置
-            this.fs.copy(this.templatePath(`${this.projectAssets}/_.babelrc`),this.destinationPath(`${this.projectName}/.babelrc`));
+            this.fs.copy(this.templatePath(`${this.projectAssets}/_.babelrc`), this.destinationPath(`${this.projectName}/.babelrc`));
 
-           //复制.gitignore
-            this.fs.copy(this.templatePath(`${this.projectAssets}/_.gitignore`),this.destinationPath(`${this.projectName}/.gitignore`));
+            //复制.gitignore
+            this.fs.copy(this.templatePath(`${this.projectAssets}/_.gitignore`), this.destinationPath(`${this.projectName}/.gitignore`));
 
-        }else if(this.projectAssets === "smobile" || this.projectAssets === "spc"){
+        } else if (this.projectAssets === "smobile" || this.projectAssets === "spc") {
             //复制模板
-            this.fs.copy(this.templatePath( `${this.projectAssets}`),this.destinationPath(`${this.projectName}`));
-            
+            this.fs.copy(this.templatePath(`${this.projectAssets}`), this.destinationPath(`${this.projectName}`));
+
             // 复制js
-            this.fs.copy(this.templatePath('common/js'),this.destinationPath(`${this.projectName}/js`));
+            this.fs.copy(this.templatePath('common/js'), this.destinationPath(`${this.projectName}/js`));
 
             //复制css
-            this.fs.copy(this.templatePath('common/css'),this.destinationPath(`${this.projectName}/css`));
+            this.fs.copy(this.templatePath('common/css'), this.destinationPath(`${this.projectName}/css`));
         }
     }
-    end(){
+
+    end() {
         console.log("运行cnpm install 安装");
-    }    
+    }
 };
